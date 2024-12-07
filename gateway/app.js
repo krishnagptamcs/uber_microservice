19b2331ad6f094f7da07b61d557dd5441a975a9c
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-12-07 11:55:52 +0530

    Gateway server added for microservice routing

---
+++ gateway/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const expressProxy = require("express-http-proxy");
+
+const app = express();
+
+//when any req. coming to user route , redirect to this port
+app.use("/user", expressProxy("http://localhost:3001"));
+
+app.listen(3000, () => {
+  console.log("Gateway server listing on port 3000");
+});
